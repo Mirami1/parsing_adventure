81cb2468b63b9e0121086ac436476ae6319e33a7
Mirami <mishaivanov99@gmail.com>
2020-11-11 14:28:31 +0500

    Добавлена запись в файл

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-import re
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote import webelement
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
+import json
 
 
 def work(url: str):
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(url)
-    driver.implicitly_wait(10) # ожидание по возможному безействию
+    driver.implicitly_wait(10)  # ожидание по возможному безействию
 
     driver.find_element_by_xpath(
         "//a[@class='header-mobile-toggle inline-block-item vertical-middle hide-for-xlarge']").click()
@@ -23,21 +19,21 @@ def work(url: str):
     driver.find_element_by_xpath(
         "//a[@class='button filter-mobile-toggle']").click()
 
-    names = ['Цвет камня', 'Камень', 'Металл'] #названия фильтров
-    data = {} # результат выборки
+    names = ['Цвет камня', 'Камень', 'Металл']  # названия фильтров
+    data = {}  # результат выборки
 
     for name in names:
 
         data[name] = {}
         elem = driver.find_element_by_xpath(
-            '//span[text()="{0}"]'.format(name)).find_element_by_xpath('./../..') # находим название категории фильтра
+            '//span[text()="{0}"]'.format(name)).find_element_by_xpath('./../..')  # находим название категории фильтра
 
         elem.click()
         time.sleep(2)
-        checkbox = elem.find_element_by_class_name('checkbox') # контейнер фильтра
-        container_1 = checkbox.find_elements_by_tag_name("label") # его элементы
+        checkbox = elem.find_element_by_class_name('checkbox')  # контейнер фильтра
+        container_1 = checkbox.find_elements_by_tag_name("label")  # его элементы
 
-        #прожимаем и отжимаем каждый элемент фильтра и заносим адрес страницы во множество
+        # прожимаем и отжимаем каждый элемент фильтра и заносим адрес страницы во множество
         for elem_1 in container_1:
             time.sleep(1)
             ActionChains(driver).move_to_element(elem_1).click(elem_1).perform()
@@ -46,17 +42,23 @@ def work(url: str):
                 "//a[@class='button filter-mobile-toggle']")
             filter_button.send_keys(Keys.CONTROL + Keys.HOME)
             time.sleep(1)
+            filter_button = driver.find_element_by_xpath(
+                "//a[@class='button filter-mobile-toggle']")
             filter_button.click()
             elem_1_text = elem_1.get_attribute("innerHTML").strip()
+
             if not elem_1_text in data[name].keys():
-                data[name][elem_1_text] = set()
-            data[name][elem_1_text].add(driver.current_url)
+                data[name][elem_1_text] = []
+            data[name][elem_1_text].append(driver.current_url)
+
             print(driver.current_url)
             ActionChains(driver).move_to_element(elem_1).click(elem_1).perform()
             time.sleep(2)
             filter_button = driver.find_element_by_xpath(
                 "//a[@class='button filter-mobile-toggle']")
             filter_button.send_keys(Keys.CONTROL + Keys.HOME)
+            filter_button = driver.find_element_by_xpath(
+                "//a[@class='button filter-mobile-toggle']")
             time.sleep(1)
             filter_button.click()
 
@@ -66,6 +68,9 @@ def work(url: str):
         unclick_button.click()
         time.sleep(2)
     print(data)
+
+    with open('result.json', 'w', encoding="windows-1251") as fp:
+        json.dump(data, fp, ensure_ascii=False, indent=4)
     driver.quit()
 
 
